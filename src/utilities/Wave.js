7c3e8ad2d692323c7ad32427dce3f37f028eefe9
Veltosian <veltosian@gmail.com>
2023-01-12 12:46:21 -0800

    Update Wave.js to reflect the current usage

--- src/utilities/Wave.js
+++ src/utilities/Wave.js
@@ -1,35 +1,16 @@
 import WaveLogicTypes from "../types/WaveLogicTypes";
 
-class Wave {
-  constructor(name, sequence = [], type = WaveLogicTypes.Clock) {
+export class Wave {
+  constructor(
+    name = "",
+    period = 1,
+    type = WaveLogicTypes.Clock,
+    sequence = []
+  ) {
     this.name = name;
     this.sequence = sequence;
     this.dependency = null;
-  }
-
-  setSequence(newSequence) {
-    if (!newSequence) {
-      throw new Error(`Invalid sequence to set to wave: ${newSequence}`);
-    }
-    this.sequence = newSequence;
-  }
-
-  clearSequence() {
-    this.sequence = [];
-  }
-
-  setDependencyFunction(dependency) {
-    if (typeof dependency !== "function") {
-      throw new Error(
-        `Invalid dependency set to wave. Type of dependency is "${typeof dependency}"`
-      );
-    }
-    this.dependency = dependency;
-
-    this.updateSequence();
-  }
-
-  updateSequence() {
-    this.sequence = this.sequence.map(this.dependency);
+    this.type = type;
+    this.period = period;
   }
 }
