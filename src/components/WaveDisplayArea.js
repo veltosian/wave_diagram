8e17dab102e0482d315d8c05464732ca1a91efcd
Veltosian <veltosian@gmail.com>
2023-01-17 08:17:52 -0800

    Fix warning

--- src/components/WaveDisplayArea.js
+++ src/components/WaveDisplayArea.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "./UI/Card";
 import SingleWaveDisplay from "./SingleWaveDisplay";
 import WaveEditDrawer from "./WaveEditDrawer";
